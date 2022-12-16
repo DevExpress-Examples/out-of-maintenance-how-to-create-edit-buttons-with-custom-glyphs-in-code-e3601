4cb70a4946d33f5bdda802ab338d27510384b565
DevExpressExampleBot <fakeEmail>
2022-12-16 23:01:34 +0400

    Source auto update [skip ci]

--- VB/DXEditors_HowToCreateButtonEdit/MainWindow.xaml.vb
+++ VB/DXEditors_HowToCreateButtonEdit/MainWindow.xaml.vb
@@ -1,4 +1,3 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
 Imports System.Windows
 Imports System.Windows.Controls
@@ -6,21 +5,23 @@ Imports System.Windows.Media.Imaging
 Imports DevExpress.Xpf.Editors
 
 Namespace DXEditors_HowToCreateButtonEdit
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
-		Public Sub New()
-			InitializeComponent()
-			CreateEditButton(btnEdit, "/DXEditors_HowToCreateButtonEdit;component\plus.png")
-		End Sub
 
-		Private Sub CreateEditButton(ByVal edit As ButtonEdit, ByVal imgUri As String)
-			Dim bitmap As New BitmapImage(New Uri(imgUri, UriKind.Relative))
-			Dim imgSource As New Image() With {.Source = bitmap}
-			Dim btnInfo As New ButtonInfo() With {.GlyphKind = GlyphKind.User, .Content = imgSource}
-			edit.Buttons.Add(btnInfo)
-		End Sub
-	End Class
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.CreateEditButton(Me.btnEdit, "/DXEditors_HowToCreateButtonEdit;component\plus.png")
+        End Sub
+
+        Private Sub CreateEditButton(ByVal edit As ButtonEdit, ByVal imgUri As String)
+            Dim bitmap As BitmapImage = New BitmapImage(New Uri(imgUri, UriKind.Relative))
+            Dim imgSource As Image = New Image() With {.Source = bitmap}
+            Dim btnInfo As ButtonInfo = New ButtonInfo() With {.GlyphKind = GlyphKind.User, .Content = imgSource}
+            edit.Buttons.Add(btnInfo)
+        End Sub
+    End Class
 End Namespace
